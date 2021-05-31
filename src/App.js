1d5cf1e3a052e20f05fed08175e10a5aadac9e88
Francisco Montenegro <francisco36@gitlab.com>
2021-05-31 20:05:54 -0300

    finished show people

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+  
+  return (
+    <main>
+      <section className="container">
+        <h3 className=""> {people.length} Birthdays Today</h3>
+        <List people={people} />
+        <button onClick={()=>{setPeople([])}}>Clear all</button>
+      </section>
+    </ main >
+  )
 }
 
 export default App;
